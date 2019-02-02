b2960ad6759acc54d71ba3019fda6ad9a7cde028
bisratyalew <bisratyalew10@gmail.com>
2019-02-02 13:51:27 -0800

    Added get_user and usernames client socket func

--- js/main.js
+++ js/main.js
@@ -18,9 +18,31 @@ $(function(){
                 $error.addClass('alert alert-danger');
                 $error.html('Username is already taken');
             }
-        });
+        });        
     
         $username.val('');
-    });    		
+    }); 
+    
+    socket.on('usernames', function(data) {
+
+        socket.on('get_user', function(user) {    
+
+            var html = '<p class="bg-primary" style="background-color: #274472">Active Users' + ' (' + data.length + ')' + '</p>';
+
+            for(i=0; i<data.length; i++) {
+                if(data[i] == user) {
+                    html += '<span class="bg-info"><span class="text-warning"> &#9679;  </span>' + '<span class="text-warning"> ' +  user + " </span>(You) &nbsp; - &nbsp;&nbsp;&nbsp;" + '<a class="text-danger" href="https://w13chat.herokuapp.com/">Disconnect</a>' + '</span><br>';
+                } else {
+                   html += '<span class="bg-success"><span class="text-success"> &#9679;  </span>' + '<span class="text-primary"> ' + data[i] + '</span></span><br>';
+                }
+            }
+
+            $users.html(html);
+
+        });
+
+        socket.emit('getUser');    
+
+    });
     
 })
