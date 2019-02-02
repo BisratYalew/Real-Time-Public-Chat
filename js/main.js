0ffb4c2423a3d498a69dc55158e15fda6bca3384
bisratyalew <bisratyalew10@gmail.com>
2019-02-02 13:43:45 -0800

    Declared html classs and ids using jquery

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+    var socket = io.connect();
+    var $messageForm = $('#messageForm');
+    var $message = $('#message');
+    var $chat = $('#chatWindow');
+    var $usernameForm = $("#UsernameForm");
+    var $users = $('#users');
+    var $username = $('#username');
+    var $error = $('#error');
+    
+})
